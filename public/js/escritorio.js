122359bd4d3df81a0afdeaf91a65dc0f03c2b7b7
lenondev <lenon.acacio@grupoonetec.com>
2023-04-11 12:11:21 +0200

    web sockets tickets app

---
+++ public/js/escritorio.js
@@ -0,0 +1,65 @@
+// Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender    = document.querySelector('button');
+const lblTicket     = document.querySelector('small');
+const divAlerta     = document.querySelector('.alert');
+const lblPendientes = document.querySelector('#lblPendientes');
+
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html'
+    throw new Error('El escritorio es obligatorio');
+    
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio;
+
+divAlerta.style.display = 'none';
+
+const socket = io();
+
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+    
+    btnAtender.disabled = false;
+
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+
+    btnAtender.disabled = true;
+});
+
+socket.on('ultimo-ticket', ( ultimo ) => {
+    // lblNuevoTicket.innerText = `Ticket ${ultimo}`;
+});
+
+socket.on('ticket-pendientes', ( tickets ) => {
+    if (tickets === 0) {
+        lblPendientes.style.display = 'none';
+    } else {
+        divAlerta.style.display = 'none';
+        lblPendientes.style.display = '';
+        lblPendientes.innerText = tickets;
+    }
+});
+
+btnAtender.addEventListener( 'click', () => {
+
+    socket.emit('atender-ticket', {escritorio}, ( {ok, ticket, msg} ) => {
+    
+        if (!ok) {
+            lblTicket.innerText = 'nadie.'
+            return divAlerta.style.display = '';
+        }
+    
+        lblTicket.innerText = `Ticket ${ticket.numero}`;
+    
+    })
+
+});
